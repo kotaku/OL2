0788c60bba156587be5d8c6ecb4f00c302fb3a50
Raman Karpechyn <raman_karpechyn@epam.com>
2020-08-31 00:05:50 +0300

    update callback

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
-function loadJSON() {
+function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', '../o3r0ivnrlj0nwkj9jr920rjb/skcj943yfhdy2uf84.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
-            return xobj.responseText;
+            callback(xobj.responseText);
         }
     };
     xobj.send(null);
 }
 
-function checkAnswer(number, answers) {
-    var actualJSON = JSON.parse(loadJSON());
-    var question = actualJSON.index[number];
+function checkAnswer(number, answers, callback) {
+    loadJSON(function (response) {
+        var actualJSON = JSON.parse(response);
+        var question = actualJSON.index[number];
 
-    if (Array.isArray(answers) &&
-        Array.isArray(question.answers) &&
-        answers.length === question.answers.length &&
-        answers.every((val, index) => val === question.answers[index])) {
-        return question.true;
-    }
-    else {
-        return question.false;
-    }
+        if (Array.isArray(answers) &&
+            Array.isArray(question.answers) &&
+            answers.length === question.answers.length &&
+            answers.every((val, index) => val === question.answers[index])) {
+            return callback(question.true);
+        }
+        else {
+            return callback(question.false);
+        }
+    });
 }
