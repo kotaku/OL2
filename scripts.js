b5cb0cf7902d7de2d2f39c447fbe902c18c28ee6
Raman Karpechyn <raman_karpechyn@epam.com>
2020-08-30 23:38:29 +0300

    fix parser

--- scripts.js
+++ scripts.js
@@ -1,23 +1,19 @@
-function loadJSON(callback) {
+function loadJSON() {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', '../o3r0ivnrlj0nwkj9jr920rjb/skcj943yfhdy2uf84.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
+            return xobj.responseText;
         }
     };
     xobj.send(null);
 }
 
 function checkAnswer(number, answers) {
-    loadJSON(function (response) {
-        actualJSON = JSON.parse(response);
-        check(answers, actual_JSON.index[number])
-    });
-}
+    var actualJSON = JSON.parse(loadJSON());
+    var question = actualJSON.index[number];
 
-function check(answers, question) {
     if (Array.isArray(answers) &&
         Array.isArray(question.answers) &&
         answers.length === question.answers.length &&
