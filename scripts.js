6b9be9bdc54d65d0622d010914f333825b27fb21
Raman Karpechyn <raman_karpechyn@epam.com>
2020-08-30 19:35:16 +0300

    fix checkAnswer

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'o3r0ivnrlj0nwkj9jr920rjb/skcj943yfhdy2uf84.json', true);
+    xobj.open('GET', '../o3r0ivnrlj0nwkj9jr920rjb/skcj943yfhdy2uf84.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
@@ -13,17 +13,16 @@ function loadJSON(callback) {
 function checkAnswer(number, answers) {
     loadJSON(function (response) {
         var actual_JSON = JSON.parse(response);
+        var question = actual_JSON.index[number];
+
+        if (Array.isArray(answers) &&
+            Array.isArray(question.answers) &&
+            answers.length === question.answers.length &&
+            answers.every((val, index) => val === question.answers[index])) {
+            return question.true;
+        }
+        else {
+            return question.false;
+        }
     });
-
-    var question = actual_JSON.index[number];
-
-    if (Array.isArray(answers) &&
-        Array.isArray(question.answers) &&
-        answers.length === question.answers.length &&
-        answers.every((val, index) => val === question.answers[index])) {
-        return question.true;
-    }
-    else {
-        return question.false;
-    }
 }
